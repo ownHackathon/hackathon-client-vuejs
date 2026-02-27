cbcfb2d451db10dc6681447a184c2ef01f760521
BibaltiK <bibaltik@exdrals.de>
2026-02-27 02:22:00 +0100

    fix(constants): update description for `PUBLIC` visibility to clarify access and listing behavior

--- src/constants/visibility.js
+++ src/constants/visibility.js
@@ -3,5 +3,5 @@ export const Visibility = Object.freeze({
   INTERNAL: {id: 2, label: 'intern', description: 'Wird nicht gelistet. Nur der Eigentümer kann Mitglieder hinzufügen. Nur Mitglieder können den Inhalt sehen.', icon: 'pi pi-eye-slash'},
   INVITE_ONLY: {id: 3, label: 'Einladung erforderlich', description: 'Wird nicht gelistet. Der Beitritt erfordert eine Einladung. Nur Mitglieder können den Inhalt sehen.', icon: 'pi pi-receipt'},
   UNLISTED: {id: 4, label: 'nicht gelistet', description: 'Mit Link frei zugänglich und einsehbar, aber erscheint in keinen Listen', icon: 'pi pi-eye-slash'},
-  PUBLIC: {id: 5, label: 'öffentlich', description: 'Jeder kann sehen', icon: 'pi pi-eye'}
+  PUBLIC: {id: 5, label: 'öffentlich', description: 'Wird gelistet und jeder kann den Inhalt einsehen und beitreten.', icon: 'pi pi-eye'}
 });
